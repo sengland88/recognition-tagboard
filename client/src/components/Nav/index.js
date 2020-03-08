5a988ad0754f90ca237a70b1348ee95c3125101c
Shelby England <Shelby.England@knights.ucf.edu>
2020-03-08 11:10:34 -0400

    worked on responsive navbar for mobile

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -18,7 +18,11 @@ class Nav extends Component {
           />
         </a>
 
-        <div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link
